6c4e1c703ee3da0d18c8735baf492bf7a963a2ad
sumitsamsharma <sharmasumit94286@gmail.com>
2020-10-16 21:42:23 +0530

    Added push pop using linked list

--- src/main/java/Stacks.java
+++ src/main/java/Stacks.java
@@ -2,17 +2,38 @@ import java.util.LinkedList;
 class Stack <T extends Comparable>
 {
     private LinkedList l = new LinkedList();
-    public void push(T a) {
+    public void push(T a)
+    {
         l.addFirst(a);
+    }
+
+    public Object top()
+    {
+        return l.getFirst();
+    }
+    public Object pop()
+    {
+        return l.removeFirst();
+    }
+    public void empty()
+    {
+        while(!l.isEmpty())
+        {
+            System.out.println("Popped elements: " + top());
+            pop();
+        }
     }
 
 }
 public class Stacks {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Stack s = new Stack();
         s.push(70);
         s.push(36);
         s.push(50);
+        System.out.println("The top element of the stack is: " + s.top());
+        s.empty();
 
     }
 }
